fc5da29277cab9a3a141e4cc5c8c1df8aca37561
Nauzet Díaz Rosales <ndiaz@aidacanarias.com>
2018-08-10 13:30:25 +0100

    Add wrapper to crud form

---
+++ CrudForm.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <div class="crudForm elevation-4" >
+       <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'crud-form'
+}
+</script>
+<style scoped>
+  .crudForm {
+    max-width: 60%;
+    width: 60%;
+  }
+  .container {
+    display: flex;
+    flex-flow: column;
+    align-items: center;
+  }
+</style>
